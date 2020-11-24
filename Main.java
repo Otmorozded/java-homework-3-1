7ba04d6f38e16defa5678951c0292352ed7b18de
Evgeny Vasilev <jony.torrent@gmail.com>
2020-11-24 14:17:40 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_value = 5000;
+        int mile_value = 20;
+        int bonus = ticket_value / mile_value;
+        System.out.println(bonus);
+    }
+}
